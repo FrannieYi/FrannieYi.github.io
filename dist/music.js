f2a465d1ca5ae53e612fe0c75b374a3e811f875e
FrannieYi <563302085@qq.com>
2020-03-23 21:39:10 +0800

    Site updated: 2020-03-23 21:38:55

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,31 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-      },
-      {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-      },
-      {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        name: '亦是此间少年',
+        artist: '枯木逢春',
+        url: 'http://music.163.com/song/media/outer/url?id=1385646451.mp3',
+        cover: '/images/alone.jpg',
       }
+	  // {
+    //     name: 'Umbrella (Matte Remix)',
+    //     artist: 'Matte / Ember Island',
+    //     url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
+    //     cover: '/images/1.jpg',
+    //   },
+	  // {
+    //     name: '大眠 (完整版)原唱：王心凌',
+    //     artist: '小乐哥',
+    //     url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
+    //     cover: '/images/2.jpg',
+    //   },
+	  // {
+    //     name: '世间美好与你环环相扣',
+    //     artist: '柏松',
+    //     url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
+    //     cover: '/images/3.jpg',
+    //   }
     ]
 });
